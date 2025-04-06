5af3583d72627ffeb859e988590807819a956bfa
JoeyOL <2419466090@qq.com>
2025-04-06 22:08:45 +0800

    代码: 完成到48题

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,28 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let mut input = input.to_string();
+    while input.ends_with(' ') {
+        input.pop();
+    }
+    while input.starts_with(' ') {
+        input.remove(0);
+    }
+    input
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    input.to_string() + " world!"
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.to_string().replace("cars", "balloons")
 }
 
 #[cfg(test)]
